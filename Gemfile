597c7d9cfe26fb6121532d9b5576d38502e331d3
Andy Gregorowicz <andy@mitre.org>
2011-08-23 16:58:34 -0400

    Getting things to work with the latest version of sprockets

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source :rubygems
-gem "rails"
-gem 'sprockets', '= 2.0.0.beta.10' 
-require 'coffee_script'
+
+gem 'execjs'
+gem 'sprockets', "~> 2.0.0.beta.14"
+gem 'coffee-script'
+gem 'uglifier'
+gem 'rake'
+gem 'tilt'
+
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
+require 'bundler/setup'
 require 'test/unit'
 require 'tilt'
+require 'coffee_script'
 require 'sprockets'
+require 'execjs'
 
 Tilt::CoffeeScriptTemplate.default_bare=true
 class QueryExecutor
@@ -9,7 +12,7 @@ class QueryExecutor
   def self.ctx 
     unless @@ctx
       @@ctx = Sprockets::Environment.new(File.expand_path("../../", __FILE__))
-      @@ctx.paths << "app/assets/javascripts"
+      @@ctx.append_path "app/assets/javascripts"
     end
     @@ctx
   end
