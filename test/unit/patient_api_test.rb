7b109bcb374c1552581ca2ea9581a6d2d57e2e11
Anne C. Kling <akling@mitre.org>
2011-09-01 14:09:48 -0400

    added test for results

--- test/unit/patient_api_test.rb
+++ test/unit/patient_api_test.rb
@@ -53,6 +53,14 @@ class PatientApiTest  < Test::Unit::TestCase
     assert_equal '46680005', @context.eval('patient.vitalSigns()[1].resultType().code()')
     assert_equal 'BP taken sitting', @context.eval('patient.vitalSigns()[1].comment()')
   end
+  
+  def test_results
+    assert_equal 1, @context.eval('patient.results().length')
+    assert_equal '104150001', @context.eval('patient.results()[0].type()[0].code()')
+    assert_equal 'SNOMED-CT', @context.eval('patient.results()[0].type()[0].codeSystemName()')
+    assert_equal 'completed', @context.eval('patient.results()[0].status()')
+    assert_equal 'Negative', @context.eval('patient.results()[0].comment()')
+  end
 
   def test_conditions
     assert_equal 2, @context.eval('patient.conditions().length')
